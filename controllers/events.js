046f4869efb92afb94d6208c472d21b604fd6eac
Robert Zettl <zettl.robert@gmail.com>
2020-01-16 15:57:52 +0100

    added crud functionality for events

--- controllers/events.js
+++ controllers/events.js
@@ -12,6 +12,7 @@ exports.getEvents = async (req, res, next) => {
 
     res.status(200).json({
       success: true,
+      count: events.length,
       data: events
     })
   } catch (err) {
@@ -75,8 +76,32 @@ exports.createEvent = async (req, res, next) => {
 // @desc      Update Event
 // @route     PUT /api/v1/events/:id
 // @access    Private
-exports.updateEvent = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Update Event ${req.params.id}` })
+exports.updateEvent = async (req, res, next) => {
+  try {
+    const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+
+    // Error Handling if not Existing
+    if (!event) {
+      return res.status(400).json({
+        success: false
+      })
+    }
+
+    res.status(200).json({
+      sucess: true,
+      data: event
+    })
+
+  } catch (err) {
+
+    res.status(400).json({
+      success: false
+    })
+  }
+
 }
 
 
@@ -84,6 +109,25 @@ exports.updateEvent = (req, res, next) => {
 // @desc      Delete Events
 // @route     DELETE /api/v1/events
 // @access    Private
-exports.deleteEvent = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Delete Event ${req.params.id}` })
+exports.deleteEvent = async (req, res, next) => {
+  try {
+    const event = await Event.findByIdAndDelete(req.params.id)
+
+    // Error Handling if not Existing
+    if (!event) {
+      return res.status(400).json({
+        success: false
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    })
+
+  } catch (error) {
+    res.status(400).json({
+      success: false
+    })
+  }
 }
