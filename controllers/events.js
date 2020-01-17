17de469fe3dbb14b1449d86963ed5edda219b809
Robert Zettl <zettl.robert@gmail.com>
2020-01-17 11:38:02 +0100

    added highOrder asyncHandler - async.js to avoid try catch in each event middleware

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Event = require('../models/Event');
 
 // Middleware Functions
@@ -7,125 +8,94 @@ const Event = require('../models/Event');
 // @desc      Get All Events
 // @route     GET /api/v1/events
 // @access    Public
-exports.getEvents = async (req, res, next) => {
-  try {
-    const events = await Event.find();
+exports.getEvents = asyncHandler(async (req, res, next) => {
+  const events = await Event.find();
 
-    res.status(200).json({
-      success: true,
-      count: events.length,
-      data: events
-    })
-  } catch (err) {
-    next(err);
-  }
-}
+  res.status(200).json({
+    success: true,
+    count: events.length,
+    data: events
+  })
 
-
+})
 
 
 
 // @desc      Get Event
 // @route     GET /api/v1/events/:id
 // @access    Public
-exports.getEvent = async (req, res, next) => {
-  try {
-    const event = await Event.findById(req.params.id);
+exports.getEvent = asyncHandler(async (req, res, next) => {
+  const event = await Event.findById(req.params.id);
 
-    // Check if ID is not in DB
-    if (!event) {
-      return next(
-        new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
-      )
-    }
-
-    res.status(200).json({
-      success: true,
-      data: event
-    })
-  } catch (err) {
-
-    // res.status(400).json({
-    //   sucess: false,
-    // }
-
-    next(err)
+  // Check if ID is not in DB
+  if (!event) {
+    return next(
+      new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
+    )
   }
-}
+
+  res.status(200).json({
+    success: true,
+    data: event
+  })
+})
 
 
 
 // @desc      Create new Event
 // @route     POST /api/v1/events
 // @access    Private
-exports.createEvent = async (req, res, next) => {
-  try {
-    const event = await Event.create(req.body);
+exports.createEvent = asyncHandler(async (req, res, next) => {
+  const event = await Event.create(req.body);
 
-    // Succesfully Created Ressource
-    res.status(201).json({
-      success: true,
-      data: event
-    })
-
-  } catch (err) {
-
-    // Respond to Error
-    next(err)
-  }
-}
+  // Succesfully Created Ressource
+  res.status(201).json({
+    success: true,
+    data: event
+  })
+})
 
 
 
 // @desc      Update Event
 // @route     PUT /api/v1/events/:id
 // @access    Private
-exports.updateEvent = async (req, res, next) => {
-  try {
-    const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    })
+exports.updateEvent = asyncHandler(async (req, res, next) => {
 
-    // Error Handling if not Existing
-    if (!event) {
-      return next(
-        new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
-      )
-    }
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
 
-    res.status(200).json({
-      sucess: true,
-      data: event
-    })
-
-  } catch (err) {
-
-    next(err)
+  // Error Handling if not Existing
+  if (!event) {
+    return next(
+      new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
+    )
   }
 
-}
+  res.status(200).json({
+    sucess: true,
+    data: event
+  })
+})
 
 
 
 // @desc      Delete Events
 // @route     DELETE /api/v1/events
 // @access    Private
-exports.deleteEvent = async (req, res, next) => {
-  try {
-    const event = await Event.findByIdAndDelete(req.params.id)
+exports.deleteEvent = asyncHandler(async (req, res, next) => {
+  const event = await Event.findByIdAndDelete(req.params.id)
 
-    // Error Handling if not Existing
-    return next(
-      new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
-    )
+  // Error Handling if not Existing
+  return next(
+    new ErrorResponse(`Event not found with id of ${req.params.id}`, 404)
+  )
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    })
+  res.status(200).json({
+    success: true,
+    data: {}
+  })
 
-  } catch (error) {
-    next(err)
-  }
-}
+})
